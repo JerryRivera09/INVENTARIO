f65bed4eb559d83f6b8676fe858b68c4a13b0df9
robinrestrepo23 <rrestrepoamaya23@gmail.com>
2026-03-12 14:44:47 -0500

    cambiando ruta API

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-let products = require('./products.json');
+let products = require("./products.json");
 
 // GET
-app.get('/products', (req, res) => {
-    res.json(products);
+app.get("/products", (req, res) => {
+  res.json(products);
 });
 
 // POST
-app.post('/products', (req, res) => {
-    const newProduct = {
-        id: Date.now(),
-        name: req.body.name,
-        price: req.body.price,
-        stock: req.body.stock
-    };
+app.post("/products", (req, res) => {
+  const newProduct = {
+    id: Date.now(),
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock,
+  };
 
-    products.push(newProduct);
-    fs.writeFileSync('./products.json', JSON.stringify(products, null, 2));
-    res.json(newProduct);
+  products.push(newProduct);
+  fs.writeFileSync("./products.json", JSON.stringify(products, null, 2));
+  res.json(newProduct);
 });
 
 // DELETE
-app.delete('/products/:id', (req, res) => {
-    products = products.filter(p => p.id != req.params.id);
-    fs.writeFileSync('./products.json', JSON.stringify(products, null, 2));
-    res.json({ message: "Producto eliminado" });
+app.delete("/products/:id", (req, res) => {
+  products = products.filter((p) => p.id != req.params.id);
+  fs.writeFileSync("./products.json", JSON.stringify(products, null, 2));
+  res.json({ message: "Producto eliminado" });
 });
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor corriendo en puerto 3000");
 });
