bcec96a7124a1ff11be9b6a01c2bb9d0d9048219
robinrestrepo23 <rrestrepoamaya23@gmail.com>
2026-03-12 14:15:02 -0500

    cambiando ruta API

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,13 @@
-const API = "http://localhost:3000/products";
+const API = "http://10.0.2.18:3000/products";
 
 async function loadProducts() {
-    const res = await fetch(API);
-    const products = await res.json();
+  const res = await fetch(API);
+  const products = await res.json();
 
-    const list = document.getElementById("productList");
-    list.innerHTML = "";
+  const list = document.getElementById("productList");
+  list.innerHTML = "";
 
-    products.forEach(p => {
+  products.forEach((p) => {
     list.innerHTML += `
         <tr>
             <td>${p.name}</td>
@@ -19,79 +19,70 @@ async function loadProducts() {
             </td>
         </tr>
     `;
-});
+  });
 }
 
 async function addProduct() {
-    const name = document.getElementById("name").value;
-    const price = document.getElementById("price").value;
-    const stock = document.getElementById("stock").value;
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const stock = document.getElementById("stock").value;
 
-    await fetch(API, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ name, price, stock })
-    });
-
-    
+  await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, price, stock }),
+  });
 }
 
-
-
 function searchProduct() {
+  const text = document.getElementById("search").value.toLowerCase();
+  const rows = document.querySelectorAll("tbody tr");
 
-    const text = document.getElementById("search").value.toLowerCase();
-    const rows = document.querySelectorAll("tbody tr");
+  rows.forEach((row) => {
+    const name = row.children[0].textContent.toLowerCase();
 
-    rows.forEach(row => {
-
-        const name = row.children[0].textContent.toLowerCase();
-
-        if (name.includes(text)) {
-            row.style.display = "";
-        } else {
-            row.style.display = "none";
-        }
-
-    });
-    loadProducts();
+    if (name.includes(text)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+  loadProducts();
 }
 //EDITAR
 async function editProduct(id) {
+  const name = prompt("Nuevo nombre:");
+  const price = prompt("Nuevo precio:");
+  const stock = prompt("Nuevo stock:");
 
-    const name = prompt("Nuevo nombre:");
-    const price = prompt("Nuevo precio:");
-    const stock = prompt("Nuevo stock:");
+  if (!name || !price || !stock) {
+    alert("Debes llenar todos los campos");
+    return;
+  }
 
-    if(!name || !price || !stock){
-        alert("Debes llenar todos los campos");
-        return;
-    }
+  await fetch(`${API}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      price: price,
+      stock: stock,
+    }),
+  });
 
-    await fetch(`${API}/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: name,
-            price: price,
-            stock: stock
-        })
-    });
-
-    loadProducts();
+  loadProducts();
 }
 
 async function deleteProduct(id) {
+  await fetch(`${API}/${id}`, {
+    method: "DELETE",
+  });
 
-    await fetch(`${API}/${id}`, {
-        method: "DELETE"
-    });
-
-    loadProducts();
+  loadProducts();
 }
 
 loadProducts();
